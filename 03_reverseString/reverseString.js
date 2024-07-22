096dee4a2d19671efbbf789ee0e811126d38933b
Wyatt <wyaruss@gmail.com>
2024-07-22 10:32:03 -0700

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let stringArray = Array.from(word); 
+    let reversedArray = stringArray.reverse(); 
+    let reversedString = stringArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
