cbf7c89e0e9f190f73c60314146f1dbe161e74b9
Wyatt <wyaruss@gmail.com>
2024-07-22 12:53:06 -0700

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    //Assign start and end based on which value is lower 
+    if (firstNum > secondNum) {
+        var start = secondNum;
+        var end = firstNum;
+    } else {
+        var start = firstNum;
+        var end = secondNum;
+    }
 
+    //Error out for negatives
+    if (firstNum < 0 || secondNum < 0) {
+        return("ERROR");
+    }
+
+    //Error out for NaNs
+    if (typeof firstNum != "number" || typeof secondNum != "number") {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
