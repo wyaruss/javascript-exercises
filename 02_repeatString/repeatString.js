a9415659984776fb24ba10fbc1c9e2422ce8c3fc
Wyatt <wyaruss@gmail.com>
2024-07-22 10:21:25 -0700

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, timesToPrint) {
+    if (timesToPrint < 0) {
+        return "ERROR";
+    }
+    returnString = "";
+    for (let i = 0; i < timesToPrint; i++) {
+        returnString += word;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
