b04ae25afe7c6112b9e86592d9f43d135ad35f97
Wyatt <wyaruss@gmail.com>
2024-07-22 15:43:51 -0700

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let tempInC = ((tempInF - 32) * (5.0 / 9.0));
+  return parseFloat(tempInC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let tempInF = (tempInC * (9.0 / 5.0) + 32);
+  return parseFloat(tempInF.toFixed(1));
 };
 
 // Do not edit below this line
